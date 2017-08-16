595a26ae7b40ce5c9a01f21b0c148c44b110c9c4
Meagan Sullivan <meagan.sullivan@byu.edu>
2017-08-16 14:09:44 -0600

    Set up express application

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
